f84997d1271c1858cdeacd100f75389dc0d520c3
DazzGranto <game2002bear@gmail.com>
2025-06-29 22:04:19 +0300

    feat: add custom server for production

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-// CUSTOM SERVER
+import polka from 'polka';
+import { handler } from '../build/handler.js';
+import { Server } from 'socket.io';
+import { registerSocketEvents } from './registerSocketEvents.js';
+
+const PORT = process.env.PORT ?? 3000;
+
+const app = polka();
+
+app.use(handler);
+
+const { server: httpServer } = app.listen(PORT, () =>
+	console.log(`> Running on http://localhost:${PORT}`)
+);
+
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => registerSocketEvents(io, socket));
